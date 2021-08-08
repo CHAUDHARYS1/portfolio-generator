1683a48be48d1ba62513ba4950d56d853beea0ab
Shital Chaudhary <shitalchau10@gmail.com>
2021-08-08 11:17:04 -0500

    add questions about projects

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
 const inquirer = require('inquirer');
-inquirer
-    .prompt([
+
+const promptUser = () => {
+return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is Github username?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some info about yourself:'
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = () => {
+    console.log("================= Add a New Project =================");
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your project name?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide info about your projects: (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]);
+};
+promptUser()
+.then(answers => console.log(answers))
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers));
+;
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 
