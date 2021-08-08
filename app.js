2b9e4007a8c417d0e394b823648756ce63783ae5
Shital Chaudhary <shitalchau10@gmail.com>
2021-08-08 11:42:43 -0500

    add logic to have multiple projects

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const inquirer = require('inquirer');
 
 const promptUser = () => {
-return inquirer.prompt([
-        {
+    return inquirer.prompt([{
             type: 'input',
             name: 'name',
             message: 'What is your name?'
@@ -20,8 +19,14 @@ return inquirer.prompt([
     ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
     console.log("================= Add a New Project =================");
+
+    // if there's no 'projects' array properrty, ceate one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
     return inquirer.prompt([
         {
             type: 'input',
@@ -56,14 +61,21 @@ const promptProject = () => {
             message: 'Would you like to enter another project?',
             default: false
         }
-    ]);
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if(projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        } else{
+            return portfolioData;
+        }
+    });
 };
 promptUser()
-.then(answers => console.log(answers))
-.then(promptProject)
-.then(projectAnswers => console.log(projectAnswers));
-;
-// const fs = require('fs');
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+    // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 
 // const [name, github] = profileDataArgs;
